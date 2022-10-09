28e2e30f4c0cf931958baee159b5bf84987aa0a9
Taranjeet Singh <tsingh219@myseneca.ca>
2022-10-09 05:10:16 -0400

    Charcter Input Source code added

---
+++ Source__codes/Char_Input.c
@@ -0,0 +1,18 @@
+/*
+//My Input Program 
+
+// This is important because Visual Studio takes scanf a danger
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void main()
+{
+	char val;
+	printf("\n\n\n\t\t\t\t\tWhat's you name, Sir/Mam ?\n\n\n\n");
+	scanf("%c", &val);
+	printf("Hi %c, Welcome to your C-Journey", val);
+
+
+	printf("\n\n\n\n\n");
+}
+*/
